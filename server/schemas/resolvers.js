c29b5438497e0043981fdc5e682cdc04a2440b3e
Jess <jess.m.jerez@gmail.com>
2023-05-09 12:32:44 -0600

    set up user query in resolvers

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -4,8 +4,16 @@ const { signToken } = require('../utils/auth');
 
 const resolvers = {
   Query: {
+    user: async (parent, args, context) => {
+        if (context.user) {
+          const userData = await User.findOne({ _id: context.user._id }).select('-__v -password');
+  
+          return userData;
+        }
+  
+        throw new AuthenticationError('Not logged in');
+      },
   },
-
   Mutation: {
   }
 };
