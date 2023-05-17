42d8734a02f2182b277d8e435cef4c6a87052a40
Jess <jess.m.jerez@gmail.com>
2023-05-17 12:24:01 -0600

    v2

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/googlebooks');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://jessmjerez:<password>@cluster0.aaxg42c.mongodb.net/');
 
 module.exports = mongoose.connection;
